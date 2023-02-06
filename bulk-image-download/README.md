9a298b7e0e002ada5abec1003c88c6f2f34ffe39
Vihar <vihar2121@gmail.com>
2023-02-06 22:31:16 +0530

    fetchImages and downloadImages merged and error resolved
    Mmerged both the commands to reduce the complexity of the script.
    The issue of the script downloading the thumbnails inside the image tag instead of the actual image from its parent anchor resolved.

--- bulk-image-download/README.md
+++ bulk-image-download/README.md
@@ -14,13 +14,6 @@
 
 ## <u>Functions Implemented</u>
 
-### fetchImages(url)
-<ul>
-    <li> The function uses <i> requests.get </i> function call to get the html text of the given url page.
-    <li> It than uses beautiful soup data structure to parse the html data to extract all <u>data-src</u> and <u>src</u> attributes from the image tags.
-    <li> It returns a list of image urls formatted using regex module to be treated as absolute url path.
-</ul>
-
 ### extractImageName(image_url)
 <ul>
     <li> The function extracts name and extension of the image file and returns the file-name. If no file exists, it returns <b>None</b>.
@@ -28,6 +21,8 @@
 
 ### downloadImages(images)
 <ul>
+    <li> The function uses <i> requests.get </i> function call to get the html text of the given url page.
+    <li> It than uses beautiful soup data structure to parse the html data to extract all <u>data-src</u> and <u>src</u> attributes from the image tags.
     <li> Using <i>get</i> function call from <i>requests</i> library, the function downloads the image data.
     <li> Image name is extracted using the <i><u>extractImageName</u></i> function.
     <li> It stores the image with the same name inside <b> images </b> directory and also shows log message regarding wether a downloading is successful with respect an image or not and stores it inside <b>logFile.txt</b>.
--- bulk-image-download/app.py
+++ bulk-image-download/app.py
@@ -4,35 +4,6 @@ from os import path, mkdir
 
 __LOGFILE = 'logFile.txt'
 
-def fetchImages(url: str) -> list[str]:
-    r = requests.get(url)
-    soup = BeautifulSoup(r.content, "html.parser")
-    
-    imgs = soup.find_all('img')
-    images = []
-    
-    # extracting the basepath for the images and the webpage
-    base_url = re.match(r"^(.*/)(.*\.html)?", url).groups()[0]
-        
-    for img in imgs:
-        # trys data-src to see if lazy load is applied or not
-        image = img.get('data-src')
-        if image is None:
-            image = img.get('src')
-            
-        # formulating complete url for the image if it is given with reference to webpage folder on server
-        if image[:4] != 'http': 
-            image = path.join(base_url, image)
-        
-        # if protocol still not specified even after joining the url, then we explicitly specify the protocol
-        if image[:4] != 'http': 
-            image = 'https:' + image
-        
-        images.append(image)
-        
-    return images
-
-
 def extractImageName(image_url:str) -> str | None:
     # image = image.replace('%20', '_')  # replace the htmlparsed blank space with underscore
     matchobj = re.findall(r'/([^/]*?)\.(svg|png|jpg|gif|jpeg|webp)', image_url) # extracting image name and extension to save the image
@@ -43,40 +14,52 @@ def extractImageName(image_url:str) -> str | None:
     return f"{matchobj[0][0]}.{matchobj[0][1]}"
 
 
-def downloadImages(images: list[str]) -> None:
-    print(f"attempting to download {len(images)} images...\n")
+def downloadImages(url: str) -> None:
+    r = requests.get(url)
     
-    logFile = open(__LOGFILE, 'w')
+    logFile = open(__LOGFILE, 'a')
     
-    for image in images:
-        print(f"trying: {image}")
-        try:
-            # getting the image data
-            res = requests.get(image, stream=True)
-            
-            # extracting the image name
-            imageName = extractImageName(image)
-            
-            if not path.exists('images/'):
-                mkdir('images')
-                
-            with open('images/' + imageName, 'wb') as image_file:
-                image_file.write(res.content)
-                image_file.close()
-            
-            logFile.write(f"[info] : Downloading Successful for image at url:\n{image}\n\n")
-            print("Downloaded Successfully\n")
-        except:
-            logFile.write(f"[error] : Failed to download the image at url:\n{image}\n\n")
-            print("Failed to Download\n")
+    if r.status_code != 200:
+        return
+    
+    soup = BeautifulSoup(r.content, "html.parser")
+    imgs = soup.find_all('img')
+    
+    if not path.exists('images/'):
+        mkdir("images/")
+    
+    for img in imgs:
+        # first checks data-src to see whether lazy-load was used or not.
+        # else checks the src tag. If none are found, it returns ''
+        imageURL = img.get('data-src', img.get('src', ''))
+        parent = img.find_parent('a')
         
-    logFile.close()
+        if parent:
+            # image was inside an anchor tag so most likely was thumbnail.
+            # better to select the link in the anchor tag. if the href field is not found,
+            # the link in kept untouched
+            imageURL = parent.get('href', imageURL)
+        
+        if not imageURL: continue # empty url
+        
+        try:
+            image_data = requests.get(imageURL) # getting the image data
+            image_name = extractImageName(imageURL) # extracting image name from the url
             
+            with open("images/" + image_name, 'wb') as imageFile:
+                imageFile.write(image_data.content)
+                imageFile.close()
+                logFile.write(f"[info] : Downloading Successful for image at url:\n{imageURL}\n\n")
+        except:
+            logFile.write(f"[error] : Failed to download the image at url:\n{imageURL}\n\n")
+            
+    logFile.close()
+
+        
 
 def __main__():
-    url = "http://m.gettywallpapers.com/cool-wallpaper/"
-    image_urls = fetchImages(url)
-    downloadImages(image_urls)
+    url = "<webpage url>"
+    downloadImages(url)
     
 
 if __name__ == "__main__":
